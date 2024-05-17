bbde29b0ae1bb86b26a3b75e2a462d61e856081a
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-05-17 23:49:51 +0000

    contact form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './home/home.component';
-
+import { ConactComponent } from './conact/conact.component';
 export const routes: Routes = [
     {path: '', redirectTo: '/home',pathMatch:'full'},
-    {path: 'home', component: HomeComponent}
+    {path: 'home', component: HomeComponent},
+    {path: 'contact', component: ConactComponent}
 
 ];
 
