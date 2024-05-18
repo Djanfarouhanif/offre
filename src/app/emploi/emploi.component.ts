cb1f2d0a2f9c9a576c58a36625e82c52347e44d7
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-05-18 08:46:49 +0000

    Emploi comp

---
+++ src/app/emploi/emploi.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-emploi',
+  standalone: true,
+  imports: [],
+  templateUrl: './emploi.component.html',
+  styleUrl: './emploi.component.css'
+})
+export class EmploiComponent {
+
+}
