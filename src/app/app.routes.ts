37c16f1824a766f387d67d1b91211fa4db396a23
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-05-18 09:36:11 +0000

    css content

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { ConactComponent } from './conact/conact.component';
+import { EmploiComponent } from './emploi/emploi.component';
+
 export const routes: Routes = [
     {path: '', redirectTo: '/home',pathMatch:'full'},
     {path: 'home', component: HomeComponent},
-    {path: 'contact', component: ConactComponent}
+    {path: 'contact', component: ConactComponent},
+    {path: 'emploi', component:EmploiComponent}
 
 ];
 
