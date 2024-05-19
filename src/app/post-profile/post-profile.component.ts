016f59bbbba38df72c642ab2c7603742165ffc20
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-05-19 20:43:20 +0000

    nav bar fonction

--- src/app/post-profile/post-profile.component.ts
+++ src/app/post-profile/post-profile.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { MessagePostComponent } from '../message-post/message-post.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-post-profile',
   standalone: true,
-  imports: [],
+  imports: [ MessagePostComponent, CommonModule],
   templateUrl: './post-profile.component.html',
-  styleUrl: './post-profile.component.css'
+  styleUrl: './post-profile.component.css' 
 })
-export class PostProfileComponent {
 
+export class PostProfileComponent {
+  currentView:string = 'post';
+  setView(view:string): void {
+    this.currentView = view
+  }
 }
