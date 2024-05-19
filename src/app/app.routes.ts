d4514450599137d9d672150f438fc078867e3063
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-05-19 17:08:52 +0000

    end the part

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,16 @@ import { HomeComponent } from './home/home.component';
 import { ConactComponent } from './conact/conact.component';
 import { EmploiComponent } from './emploi/emploi.component';
 import { PostDetailleComponent } from './post-detaille/post-detaille.component';
+import { PostProfileComponent } from './post-profile/post-profile.component';
+
 
 export const routes: Routes = [
     {path: '', redirectTo: '/home',pathMatch:'full'},
     {path: 'home', component: HomeComponent},
     {path: 'contact', component: ConactComponent},
     {path: 'emploi', component:EmploiComponent},
-    {path: 'post-detaille', component: PostDetailleComponent}
+    {path: 'post-detaille', component: PostDetailleComponent},
+    { path: 'post-profile', component: PostProfileComponent}
 
 ];
 
---
+++ src/app/post-profile/post-profile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-profile',
+  standalone: true,
+  imports: [],
+  templateUrl: './post-profile.component.html',
+  styleUrl: './post-profile.component.css'
+})
+export class PostProfileComponent {
+
+}
