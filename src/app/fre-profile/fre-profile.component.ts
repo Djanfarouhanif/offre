4750d3431a39ff44cd93f9db3ca3f5eb0537dac4
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-05-24 07:14:28 +0000

    freelincing messagerie

--- src/app/fre-profile/fre-profile.component.ts
+++ src/app/fre-profile/fre-profile.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common'
 @Component({
   selector: 'app-fre-profile',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule],
   templateUrl: './fre-profile.component.html',
   styleUrl: './fre-profile.component.css'
 })
 export class FreProfileComponent {
 
+  current:string = "post";
+
+  setView(view:string):void{
+      this.current = view;
+  };
 }
